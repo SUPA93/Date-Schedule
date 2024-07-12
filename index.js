8559d92a1962a15ccc955875b99de3b782677c62
SUPA93 <supaflavaz93@hotmail.com>
2024-07-12 17:16:13 +0200

    fixes date

---
+++ index.js
@@ -0,0 +1,24 @@
+//convert today date to input format
+const today = new Date().toISOString().split("T")[0];
+
+start_date.value = today;
+start_date.min = today;
+
+// Tommoroy date calc
+let tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+///convert to input format
+let tomorrowFormat = tomorrow.toISOString().split("T")[0];
+end_date.value = tomorrowFormat;
+end_date.min = tomorrowFormat;
+
+start_date.addEventListener("change", (e) => {
+    let day = new Date(e.target.value);
+
+    if (end_date.value < start_date.value) {
+        day.setDate(day.getDate() + 1);
+        end_date.value =  day.toISOString().split("T")[0];
+    }
+
+})
